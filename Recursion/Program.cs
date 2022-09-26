c6b137b6e542f6fbe8ee336e114529e34c1b6fd6
fulfordcs <fulfordcs@gmail.com>
2022-09-26 14:49:47 +0100

    User input

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -10,6 +10,19 @@ namespace Recursion
     {
         static void Main(string[] args)
         {
+            // 5! means 5 x 4 x 3 x 2 x 1
+
+
+            // ask the user to enter n (an integer)
+            Console.WriteLine("Welcome to Factorial Factory");
+            Console.Write("Please enter a number: ");
+            int n = 0;
+            while(!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.Write("Please enter a number: ");
+            }
+
+
         }
     }
 }
